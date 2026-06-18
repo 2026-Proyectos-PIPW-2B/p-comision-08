b23da4270903860d5bc774f40383ecdc5d083146
Martin Becchio <tincho.becchio@gmail.com>
2026-06-18 20:30:37 -0300

    creacion por defecto de usuario admin

--- js/login.js
+++ js/login.js
@@ -1,4 +1,9 @@
-import { crearSesion, cerrarSesion, redirigir, autorizacion } from "./gestores/gestorLogin.js";
+import {
+  crearSesion,
+  cerrarSesion,
+  redirigir,
+  autorizacion,
+} from "./gestores/gestorLogin.js";
 import { buscarUsuario } from "./gestores/gestorUsuarios.js";
 
 const formLogin = document.getElementById("formLogin");
@@ -6,44 +11,62 @@ const inputNombre = document.getElementById("inputNombre");
 const inputContrasenia = document.getElementById("inputContrasenia");
 
 window.addEventListener("load", () => {
-    autorizacion()
+  autorizacion();
+  cargarUsuarioAdmin();
 
-    formLogin.addEventListener("submit", (e) => {
-        e.preventDefault();
+  formLogin.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        const validacion = validarFormLogin();
-        if (validacion.resultado) {
-            formLogin.reset();
-            iniciarSesion(validacion.obj);
-        }
-    });
+    const validacion = validarFormLogin();
+    if (validacion.resultado) {
+      formLogin.reset();
+      iniciarSesion(validacion.obj);
+    }
+  });
 });
 
 function validarFormLogin() {
-    const nombre = inputNombre.value;
-    const contrasenia = inputContrasenia.value;
-    const validacion = { resultado: false };
+  const nombre = inputNombre.value;
+  const contrasenia = inputContrasenia.value;
+  const validacion = { resultado: false };
 
-    const usuario = buscarUsuario(nombre);
-    if (usuario) {
-        if (usuario.contrasenia === contrasenia) {
-            validacion.resultado = true;
-            validacion.obj = { nombre: usuario.nombre, rol: usuario.rol };
-        } else {
-            feedback("Datos inválidos");
-        }
+  const usuario = buscarUsuario(nombre);
+  if (usuario) {
+    if (usuario.contrasenia === contrasenia) {
+      validacion.resultado = true;
+      validacion.obj = { nombre: usuario.nombre, rol: usuario.rol };
     } else {
-        feedback("El usuario no se encuentra registrado");
+      feedback("Datos inválidos");
     }
+  } else {
+    feedback("El usuario no se encuentra registrado");
+  }
 
-    return validacion;
+  return validacion;
 }
 
 function iniciarSesion(usuario) {
-    crearSesion(usuario.nombre, usuario.rol);
-    redirigir(usuario.rol);
+  crearSesion(usuario.nombre, usuario.rol);
+  redirigir(usuario.rol);
 }
 
 function feedback(msj) {
-    alert(msj);
+  alert(msj);
+}
+
+function cargarUsuarioAdmin() {
+  const userAdmin = buscarUsuario("admin");
+  if (!userAdmin || !userAdmin.estado) {
+    localStorage.setItem(
+      "usuarios",
+      JSON.stringify([
+        {
+          nombre: "admin",
+          contrasenia: "admin",
+          rol: "Administrador",
+          estado: true,
+        },
+      ]),
+    );
+  }
 }
