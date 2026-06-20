5726258544aa1dc3ce7caba2d56f23a05bf57abc
tinchobecchio <tincho.becchio@gmail.com>
2026-06-20 04:43:43 -0300

    #27 maquetado de formulario, modal, avance en funciones y agrega imagenes precargadas

--- js/gestores/gestorProductos.js
+++ js/gestores/gestorProductos.js
@@ -1,4 +1,60 @@
-export function modificarProducto(prod) {
-  console.log(prod);
-  console.log("holaaa");
+
+
+//   {
+  //   idProd: "123",
+  //   nombre: "alfajor rasta",
+  //   descripcion: "alfajor nacional artesanal",
+//   precioMinorista: 1200,
+//   precioMayorista: 1000,
+//   cantmin: 10,
+//   stock: 300,
+//   categoria: "alfajor",
+//   etiquetas: ["idEtiqueta1"],
+//   imagen: "",
+// }
+
+export function agregarProducto(producto) {
+  const listadoProductos = traerTodosLosProductos();
+  const nuevoProd = producto
+  nuevoProd.id = crearID()
+  listadoProductos.push(nuevoProd);
+  actualizarListadoProductos(listadoProductos);
+}
+
+function actualizarListadoProductos(arregloNuevo) {
+  localStorage.setItem("productos", JSON.stringify(arregloNuevo));
+}
+
+export function traerTodosLosProductos() {
+  return JSON.parse(localStorage.getItem("productos")) || [];
+}
+
+export function buscarProducto(id) {
+  const listadoProductos = traerTodosLosProductos();
+  return listadoProductos.find((prod) => prod.id === id);
+}
+
+export function modificarProducto(id, nuevoProd) {
+  const listadoProductos = traerTodosLosProductos();
+  const producto = listadoProductos.find((prod) => prod.id === id);
+  if (producto) {
+    producto.nombre = nuevoProd.nombre
+    producto.descripcion = nuevoProd.descripcion
+    producto.precioMinorista = nuevoProd.precioMinorista
+    producto.precioMayorista = nuevoProd.precioMayorista
+    producto.cantmin = nuevoProd.cantmin
+    producto.stock = nuevoProd.stock
+    producto.categoria = nuevoProd.categoria
+    producto.etiquetas = nuevoProd.etiquetas
+    producto.imagen = nuevoProd.imagen
+  }
+  actualizarListadoProductos(listadoProductos)
+}
+
+export function eliminarProducto(id) {
+  const listadoProductos = traerTodosLosProductos();
+  const nuevaLista = listadoProductos.filter(
+    (prod) => prod.id !== id,
+  );
+  actualizarListadoProductos(nuevaLista);
 }
--- js/productos.js
+++ js/productos.js
@@ -2,13 +2,28 @@ import { autorizacion } from "./gestores/gestorLogin.js";
 import { modificarProducto } from "./gestores/gestorProductos.js";
 import { cargarDatosNavbar } from "./navbar.js";
 
-const modalInputNombre = document.getElementById("modalInputNombre");
-const modalInputDescripcion = document.getElementById("modalInputDescripcion");
-const modalInputPrecio = document.getElementById("modalInputPrecio");
-const modalInputStock = document.getElementById("modalInputStock");
-const modalSelectCategoria = document.getElementById("modalSelectCategoria");
-const modalSelectEtiqueta = document.getElementById("modalSelectEtiqueta");
-const btnGuardarCambios = document.getElementById("btnGuardarCambios");
+const formRegistrarProducto = document.getElementById("formRegistrarProducto");
+const inputNombre = document.getElementById("inputNombre");
+const inputDescripcion = document.getElementById("inputDescripcion");
+const inputPrecioMinorista = document.getElementById("inputPrecioMinorista");
+const inputPrecioMayorista = document.getElementById("inputPrecioMayorista");
+const inputCantMay = document.getElementById("inputCantMay");
+const inputStock = document.getElementById("inputStock");
+const selectCategoria = document.getElementById("selectCategoria");
+const checkEtiquetas = document.querySelectorAll(".check-etiqueta");
+const selectImagen = document.getElementById("selectImagen");
+const inputImgURL = document.getElementById("inputImgURL");
+const imgPreview = document.getElementById("imgPreview");
+
+// const modalInputNombre = document.getElementById("modalInputNombre");
+// const modalInputDescripcion = document.getElementById("modalInputDescripcion");
+// const modalInputPrecioMinorista = document.getElementById("modalInputPrecioMinorista");
+// const modalInputPrecioMayorista = document.getElementById("modalInputPrecioMayorista");
+// const modalinputCantMay = document.getElementById("modalinputCantMay");
+// const modalInputStock = document.getElementById("modalInputStock");
+// const modalSelectCategoria = document.getElementById("modalSelectCategoria");
+
+// const btnModalGuardarCambios = document.getElementById("btnGuardarCambios");
 
 const productoEj = {
   idProd: "123",
@@ -16,57 +31,117 @@ const productoEj = {
   descripcion: "alfajor nacional artesanal",
   precioMinorista: 1200,
   precioMayorista: 1000,
+  cantmin: 10,
   stock: 300,
   categoria: "alfajor",
-  etiquetas: "idEtiqueta1",
+  etiquetas: ["idEtiqueta1"],
   imagen: "",
 };
 
 window.addEventListener("load", () => {
   autorizacion("Administrador");
   cargarDatosNavbar();
-  cargarBoton();
-  btnGuardarCambios.addEventListener("click", guardarCambios);
+  inicializar();
 });
 
-function cargarProducto(prod) {
-  modalInputNombre.value = prod.nombre;
-  modalInputDescripcion.value = prod.descripcion;
-  modalInputPrecio.value = prod.precioMinorista;
-  modalInputStock.value = prod.stock;
-  modalSelectCategoria.value = prod.categoria;
-  modalSelectEtiqueta.value = prod.etiquetas;
-}
-function editarProd() {
-  cargarProducto(productoEj);
+function inicializar() {
+  cargarImagenVistaPrevia();
+  selectImagen.addEventListener("change", cargarImagenVistaPrevia);
+  inputImgURL.addEventListener("input", cargarImagenVistaPrevia);
+
+  formRegistrarProducto.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // limpiarEstados()
+
+    const validacion = validarFormRegistrarProd();
+    if (validacion.resultado) {
+      // formRegistrarProducto.reset()
+      // limpiarEstados()
+      // registrarProducto(validacion.obj)
+      // listarProductos()
+      // mensajeExitoso()
+    }
+  });
 }
 
-function cargarBoton() {
-  const tdBoton = document.getElementById("td-boton");
-  const btn = document.createElement("button");
-  btn.classList.add("btn", "btn-primary");
-  btn.setAttribute("data-bs-toggle", "modal");
-  btn.setAttribute("data-bs-target", "#modalFormulario");
-  btn.innerHTML = `<i class="bi bi-pencil-square"></i> Editar`;
-  btn.addEventListener("click", editarProd);
-  tdBoton.appendChild(btn);
+function cargarImagenVistaPrevia() {
+  let urlPersonalizada = inputImgURL.value;
+  let nombreImagen = selectImagen.value;
+  let urlImg;
+  let alt = "imagen del producto";
+
+  // si existe prioriza el URL del input antes que el select
+  if (urlPersonalizada !== "") {
+    urlImg = urlPersonalizada;
+  } else {
+    if (nombreImagen === "sin-foto") {
+      urlImg = `/img/sin-foto.jpg`;
+    } else {
+      urlImg = `/img/${nombreImagen}.png`;
+    }
+    alt = nombreImagen
+  }
+  imgPreview.setAttribute("src", urlImg);
+  imgPreview.setAttribute("alt", alt);
 }
 
-function guardarCambios() {
-  const nombre = modalInputNombre.value;
-  const descripcion = modalInputDescripcion.value;
-  const precio = modalInputPrecio.value;
-  const stock = modalInputStock.value;
-  const categoria = modalSelectCategoria.value;
-  const etiquetas = modalSelectEtiqueta.value;
+function limpiarEstados() {}
 
-  const prodActualizado = {
-    nombre,
-    descripcion,
-    precio,
-    stock,
-    categoria,
-    etiquetas,
-  };
-  modificarProducto(prodActualizado);
+function registrarProducto(prod) {}
+
+function mensajeExitoso() {}
+
+function validarFormRegistrarProd() {
+  const validacion = { resultado: true };
+
+  return validacion;
 }
+
+// function inicializar() {
+//   cargarBoton();
+//   btnModalGuardarCambios.addEventListener("click", guardarCambios);
+// }
+
+// function cargarProductoEnModal(prod) { // carga el prod en el modal
+//   modalInputNombre.value = prod.nombre;
+//   modalInputDescripcion.value = prod.descripcion;
+//   modalInputPrecioMinorista.value = prod.precioMinorista;
+//   modalInputPrecioMayorista.value = prod.precioMayorista;
+//   modalinputCantMay.value = prod.cantmin;
+//   modalInputStock.value = prod.stock;
+//   modalSelectCategoria.value = prod.categoria;
+//   // modalSelectEtiqueta.value = prod.etiquetas;
+// }
+// function editarProd() {
+//   cargarProductoEnModal(productoEj);
+// }
+
+// function cargarBoton() { // esta se va cuando haga la de listar prod, ahi se le agrega al boton editar de c/u
+//   const tdBoton = document.getElementById("td-boton");
+//   const btn = document.createElement("button");
+//   btn.classList.add("btn", "btn-primary");
+//   btn.setAttribute("data-bs-toggle", "modal");
+//   btn.setAttribute("data-bs-target", "#modalFormulario");
+//   btn.innerHTML = `<i class="bi bi-pencil-square"></i> Editar`;
+//   btn.addEventListener("click", editarProd);
+//   tdBoton.appendChild(btn);
+// }
+
+// function guardarCambios() {
+//   const nombre = modalInputNombre.value;
+//   const descripcion = modalInputDescripcion.value;
+//   const precio = modalInputPrecio.value;
+//   const stock = modalInputStock.value;
+//   const categoria = modalSelectCategoria.value;
+//   const etiquetas = modalSelectEtiqueta.value;
+
+//   const prodActualizado = {
+//     nombre,
+//     descripcion,
+//     precio,
+//     stock,
+//     categoria,
+//     etiquetas,
+//   };
+//   modificarProducto(prodActualizado);
+// }
