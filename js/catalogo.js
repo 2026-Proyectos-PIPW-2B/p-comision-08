0fdf537053c533eceb15576986595f86e87c2cf4
tinchobecchio <tincho.becchio@gmail.com>
2026-06-29 14:29:30 -0300

    #12 funcion de filtrar productos en el catalogo

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,19 +1,27 @@
-import { buscarCategoriaPorID } from "./gestores/gestorCategorias.js";
+import { buscarCategoriaPorID, traerTodasLasCategorias } from "./gestores/gestorCategorias.js";
 import { buscarEtiquetaPorID } from "./gestores/gestorEtiquetas.js";
 import { autorizacion } from "./gestores/gestorLogin.js";
 import { traerTodosLosProductos } from "./gestores/gestorProductos.js";
 import { cargarDatosNavbar } from "./navbar.js";
 
 const contenedorProductos = document.getElementById("contenedorProductos");
+const formFiltrar = document.getElementById("formFiltrar");
+const inputBusquedaNombre = document.getElementById("inputBusquedaNombre");
+const selectCategoria = document.getElementById("selectCategoria");
 
 window.addEventListener("load", () => {
   autorizacion("Usuario");
   cargarDatosNavbar();
   listarTodosLosProductos();
+  cargarFiltrado()
 });
 
 function listarTodosLosProductos() {
   const listado = traerTodosLosProductos();
+  listarProductos(listado)
+}
+
+function listarProductos(listado) {
   contenedorProductos.innerHTML = "";
   for (const prod of listado) {
     const tarjetaProd = crearTarjetaProd(prod);
@@ -21,7 +29,7 @@ function listarTodosLosProductos() {
   }
   agregarPopoversDeBootstrap()
 }
-
+  
 function agregarPopoversDeBootstrap() {
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
   const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
@@ -88,7 +96,7 @@ function crearTarjetaProd(prod) {
   const divColMinorista = document.createElement("div");
   divColMinorista.classList.add("col-6");
   const divBoxMinorista = document.createElement("div");
-  divBoxMinorista.classList.add("p-2", "border", "rounded", "border-success");
+  divBoxMinorista.classList.add("p-2", "border", "rounded", "border-rosa");
   divBoxMinorista.setAttribute("id", `box-min-${prod.id}`)
   const spanLabelMin = document.createElement("span");
   spanLabelMin.classList.add("d-block", "small", "text-pink");
@@ -173,9 +181,7 @@ function crearTarjetaProd(prod) {
   const btnAgregarAlCarrito = document.createElement("button");
   btnAgregarAlCarrito.classList.add("btn", "btn-primary", "btn-sm", "w-100", "shadow-sm");
   btnAgregarAlCarrito.innerHTML = `<i class="bi bi-cart-plus me-1"></i> Añadir`;
-  btnAgregarAlCarrito.addEventListener("click", () => {
-    alert(`Añadido al carrito: ${prod.nombre} x${inputCant.value}`);
-  });
+  btnAgregarAlCarrito.addEventListener("click", () => agregarProdAlCarrito(prod, inputCant));
   divColBtnAgregar.appendChild(btnAgregarAlCarrito);
 
   divRowControles.append(divColInput, divColBtnAgregar);
@@ -205,17 +211,17 @@ function crearTarjetaProd(prod) {
   return divCol;
 }
 
-  function generarEtiquetas(idsEtiqEnProd, contenedor) {
-    idsEtiqEnProd.forEach(id => {
-      const etiqueta = buscarEtiquetaPorID(id)
-      const span = document.createElement("span");
-      span.classList.add("badge", "bg-light", "text-secondary", "border", "rounded-pill", "extra-small", "px-2", "me-1");
-      span.textContent = `#${etiqueta.nombre}`;
-      span.setAttribute("data-bs-toggle", "tooltip");
-      span.setAttribute("data-bs-title", etiqueta.descripcion);
-      contenedor.appendChild(span)
-    });
-  }
+function generarEtiquetas(idsEtiqEnProd, contenedor) {
+  idsEtiqEnProd.forEach(id => {
+    const etiqueta = buscarEtiquetaPorID(id)
+    const span = document.createElement("span");
+    span.classList.add("badge", "bg-light", "text-secondary", "border", "rounded-pill", "extra-small", "px-2", "me-1");
+    span.textContent = `#${etiqueta.nombre}`;
+    span.setAttribute("data-bs-toggle", "tooltip");
+    span.setAttribute("data-bs-title", etiqueta.descripcion);
+    contenedor.appendChild(span)
+  });
+}
 
 function modificarCantidad(prod, operacion) {
   const { id, cantMayorista, precioMinorista, precioMayorista, stock } = prod;
@@ -248,11 +254,11 @@ function destacarPrecio(prod, cant) {
   const boxMin = document.getElementById(`box-min-${id}`)
   const boxMay = document.getElementById(`box-may-${id}`)
   if (cant >= cantMayorista) {
-    boxMay.classList.add("border-success");
-    boxMin.classList.remove("border-success");
+    boxMay.classList.add("border-rosa");
+    boxMin.classList.remove("border-rosa");
   } else {
-    boxMay.classList.remove("border-success");
-    boxMin.classList.add("border-success");
+    boxMay.classList.remove("border-rosa");
+    boxMin.classList.add("border-rosa");
   }
 }
 
@@ -262,5 +268,41 @@ function actualizarSubTotal(span, cantidad, cantMin, pMin, pMay) {
 }
 
 // - agregarProductoAlCarrito
-//   - toma la cantidad del input y
+function agregarProdAlCarrito(prod, input) {
+  // llamar a la funcion del gestor carrito para agregar (ponerla cuando fabri mergee)
+  // agregarProdAlCarrito(prod,input.cant)
+
+  // mensaje exitoso
+  alert(`Añadido al carrito: ${prod.nombre} x${input.value}`)
+  
+  // resetear la tarjeta
+  input.value = 1
+  modificarCantidad(prod)
+}
+
+
 // - filtrarProductos
+function cargarFiltrado() {
+  const categorias = traerTodasLasCategorias()
+  for (const categ of categorias) {
+    const opt = document.createElement("option")
+    opt.setAttribute("value", categ.id)
+    opt.textContent = categ.nombre
+    selectCategoria.appendChild(opt)
+  }
+  inputBusquedaNombre.addEventListener("input", filtrarProductos)
+  selectCategoria.addEventListener("change", filtrarProductos)
+}
+
+function filtrarProductos() {
+  const nombre = inputBusquedaNombre.value.trim().toLowerCase()
+  const categoria = selectCategoria.value 
+  const listadoProductos = traerTodosLosProductos()  
+  
+  let filtrado = listadoProductos.filter(p => p.nombre.toLowerCase().includes(nombre))
+  if (categoria) {
+    filtrado = filtrado.filter(p => p.categoria === categoria)
+  }
+    
+  listarProductos(filtrado)
+}
