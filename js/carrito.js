72f19bed97f07c6c6eeb5f4146fb41772f3ed469
tinchobecchio <tincho.becchio@gmail.com>
2026-06-30 16:51:00 -0300

    funcion de crear tarjeta de producto en el listado del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -7,8 +7,8 @@ import {
 } from "./gestores/gestorCarrito.js";
 import { buscarProducto } from "./gestores/gestorProductos.js";
 
-const btnFinalizar = document.querySelector(".btn-success");
-const btnVaciar = document.querySelector(".card .btn-danger");
+const btnFinalizar = document.getElementById("btnFinalizarCompra");
+const btnVaciar = document.getElementById("btnVaciarCarrito");
 
 window.addEventListener("load", () => {
   autorizacion("Usuario");
@@ -27,7 +27,7 @@ function mostrarCarrito() {
   const contenedorCarrito = document.getElementById("contenedorCarrito");
   const carrito = traerCarrito();
 
-  //   contenedorCarrito.innerHTML = "";
+    contenedorCarrito.innerHTML = "";
 
   if (!carrito || carrito.productos.length === 0) {
     rowCarrito.classList.add("justify-content-center");
@@ -46,7 +46,7 @@ function mostrarCarrito() {
     for (const producto of carrito.productos) {
       const prod = buscarProducto(producto.idProd);
       const card = crearTarjeta(prod, producto.cantidad);
-      //   contenedorCarrito.appendChild(card);
+        contenedorCarrito.appendChild(card);
     }
 
     actualizarResumen();
@@ -54,10 +54,92 @@ function mostrarCarrito() {
 }
 
 function crearTarjeta(prod, cant) {
-  console.log(prod, cant);
+  const { id, nombre, cantMayorista, precioMinorista, precioMayorista, stock, imagenURL } = prod;
+  
+  // que precio aplica y subtotal
+  const esMayorista = cant >= cantMayorista;
+  const precioUnitario = esMayorista ? precioMayorista : precioMinorista;
+  const subtotal = precioUnitario * cant;
+
+  // crear card
   const card = document.createElement("div");
-  card.classList.add("card");
-  card.textContent = prod.nombre;
+  card.classList.add("card", "border-0", "shadow-sm", "p-3", "rounded-4", "bg-white", "mb-3", "position-relative");
+
+  card.innerHTML = `
+    <button class="btn text-danger border-0 bg-transparent position-absolute bottom-0 end-0 m-2 btn-eliminar" title="Eliminar">
+      <i class="bi bi-trash3-fill fs-5"></i>
+    </button>
+    
+    <div class="row align-items-center g-3">
+      <div class="col-4 col-sm-2 text-center">
+        <div class="bg-light p-2 rounded-3 d-flex align-items-center justify-content-center divImagen" >
+          <img src="${imagenURL}" alt="${nombre}" class="img-fluid object-fit-contain" />
+        </div>
+      </div>
+
+      <div class="col-8 col-sm-5">
+        <h3 class="h6 mb-1 fw-semibold text-dark text-capitalize text-start">${nombre}</h3>
+        <div class="d-flex align-items-center gap-2">
+          <span class="text-secondary extra-small">Precio minorista:</span>
+          <span class="fw-semibold ${!esMayorista ? 'text-pink' : 'text-dark'} small">$${precioMinorista}</span>
+        </div>
+        <div class="d-flex align-items-center gap-2">
+          <span class="text-secondary extra-small">Precio mayorista:</span>
+          <span class="fw-semibold ${esMayorista ? 'text-pink' : 'text-dark'} small">$${precioMayorista}</span>
+        </div>
+      </div>
+
+      <div class="col-12 col-sm-5 d-flex justify-content-between align-items-center">
+        <div class="d-flex flex-column align-items-center justify-content-center gap-2">
+          <div class="input-group input-group-sm">
+            <button class="btn btn-outline-primary btn-restar" type="button">-</button>
+            <input class="form-control text-center fw-bold shadow-none" type="number" value="${cant}" min="1" id="input-${id}" />
+            <button class="btn btn-outline-primary btn-sumar" type="button">+</button>
+          </div>
+          <small class="text-muted extra-small">Disponibles: <span class="fw-bold">${stock} u.</span></small>
+        </div>
+
+        <div class="text-center d-flex justify-content-center align-self-start">
+          <span class="h5 m-0 fw-bold text-success">$${subtotal}</span>
+        </div>
+      </div>
+
+    </div>
+  `;
+
+  // agregar eventos
+  const btnRestar = card.querySelector(".btn-restar");
+  const btnSumar = card.querySelector(".btn-sumar");
+  const input = card.querySelector("input[type='number']");
+  const btnEliminar = card.querySelector(".btn-eliminar");
+
+  btnRestar.addEventListener("click", () => cambiarCantidad(id, -1));
+  btnSumar.addEventListener("click", () => {
+    if (cant < stock) {
+      cambiarCantidad(id, 1);
+    } else {
+      alert("Llegaste al límite del stock disponible.");
+    }
+  });
+
+  input.addEventListener("change", () => {
+    let nuevaCant = Number(input.value);
+    if (nuevaCant < 1) {
+      nuevaCant = 1;
+      input.value = nuevaCant
+    }
+    if (nuevaCant > stock) {
+      nuevaCant = stock;
+      input.value = stock
+    }
+    
+    const diferencia = nuevaCant - cant;
+    if (diferencia !== 0) {
+      cambiarCantidad(id, diferencia);
+    }
+  });
+
+  btnEliminar.addEventListener("click", () => eliminarProductoDelCarrito(id));
 
   return card;
 }
@@ -81,21 +163,23 @@ function actualizarResumen() {
     carrito.productos.forEach((item) => {
       const infoProducto = buscarProducto(item.idProd);
       if (infoProducto) {
-        const subtotalProducto = infoProducto.precioMinorista * item.cantidad;
+        const aplicaMayorista = item.cantidad >= infoProducto.cantMayorista;
+        const precioAplicado = aplicaMayorista ? infoProducto.precioMayorista : infoProducto.precioMinorista;
+        const subtotalProducto = precioAplicado * item.cantidad;
+        
         total += subtotalProducto;
 
         resumenHtml += `
-                <div class="d-flex justify-content-between my-2 small">
-                    <span>${infoProducto.nombre} (x${item.cantidad})</span>
-                    <span>$${subtotalProducto}</span>
-                </div>
-                `;
+          <div class="d-flex justify-content-between my-2 small">
+              <span>${infoProducto.nombre} (x${item.cantidad})</span>
+              <span>$${subtotalProducto}</span>
+          </div>
+          `;
       }
     });
   }
 
-  contenedorResumen.innerHTML =
-    resumenHtml || '<p class="text-muted small">No hay productos</p>';
+  contenedorResumen.innerHTML = resumenHtml;
   totalCarrito.innerHTML = `$${total}`;
 }
 
