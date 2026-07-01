ef3bededc603817a680f7aa01beba9fc644f68e8
tinchobecchio <tincho.becchio@gmail.com>
2026-07-01 02:53:04 -0300

    #8 navbar dinamica (carga los links y el mensaje de bienvenida) y estilizada

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,44 @@
 import { cerrarSesion, obtenerUsuarioAutenticado } from "./gestores/gestorLogin.js";
 
 export function cargarDatosNavbar() {
-    const divBienvenida = document.getElementById("divBienvenida")
-    const btnCerrarSesion = document.getElementById("btnCerrarSesion")
-    btnCerrarSesion.addEventListener("click", cerrarSesion)
-    const usuarioAutenticado = obtenerUsuarioAutenticado()
-    if(usuarioAutenticado.rol === "Administrador") {
-        divBienvenida.innerHTML = `<span class="badge text-bg-secondary">${usuarioAutenticado.rol}</span> Bienvenid@ ${usuarioAutenticado.nombre}`
+    const contenedorLinks = document.getElementById("contenedorLinks");
+    const divBienvenida = document.getElementById("divBienvenida");
+    const btnCerrarSesion = document.getElementById("btnCerrarSesion");
+    const { nombre, rol } = obtenerUsuarioAutenticado();
+    let links;
+    let bienvenida
+
+    if (rol === "Administrador") {
+        bienvenida = `<span class="badge text-bg-warning">${rol}</span> Bienvenid@ ${nombre}`;
+        links = `
+        <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">${crearLink("usuarios.html", "Usuarios")}</li>
+            <li class="nav-item">${crearLink("productos.html", "Productos")}</li>
+            <li class="nav-item">${crearLink("categorias.html", "Categorías")}</li>
+            <li class="nav-item">${crearLink("etiquetas.html", "Etiquetas")}</li>
+        </ul>
+        `;
     } else {
-        divBienvenida.innerHTML = `Bienvenid@ ${usuarioAutenticado.nombre}`
+        bienvenida = `Bienvenid@ ${nombre}`;
+        links = `
+        <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">${crearLink("catalogo.html", "Catálogo")}</li>
+            <li class="nav-item">${crearLink("carrito.html", "Carrito")}</li>
+        </ul>
+        `;
+    }
+    
+    divBienvenida.innerHTML = bienvenida
+    contenedorLinks.innerHTML = links;
+    
+    btnCerrarSesion.addEventListener("click", cerrarSesion);
+}
+
+function crearLink(direccion, pagina) {
+    const esActivo = location.pathname.includes(direccion);
+    if (esActivo) {
+        return `<a class="nav-link active" aria-current="page" href="#">${pagina}</a>`;
+    } else {
+        return `<a class="nav-link" href="./${direccion}">${pagina}</a>`;
     }
 }
